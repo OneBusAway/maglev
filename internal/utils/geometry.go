f82972a760b12065b10b86b524c541e20883d689
Mohamed Adel Kamal <raynowing007@gmail.com>
2026-01-05 08:03:24 +0200

    fix: limit Exceeded now using maxCount to find its value, OutOfBounds calculation

--- internal/utils/geometry.go
+++ internal/utils/geometry.go
@@ -63,3 +63,11 @@ func CalculateBoundsFromSpan(lat, lon, latOffset, lonOffset float64) CoordinateB
 		MaxLon: lon + lonOffset,
 	}
 }
+
+// IsOutOfBounds return true only if fully out of bounds
+func IsOutOfBounds(inner, outer CoordinateBounds) bool {
+	return inner.MaxLat < outer.MinLat ||
+		inner.MinLat > outer.MaxLat ||
+		inner.MaxLon < outer.MinLon ||
+		inner.MinLon > outer.MaxLon
+}
