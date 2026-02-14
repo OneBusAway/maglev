b3a7e100834d24858f68a812e2ae14e779bd3d51
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2026-02-13 19:09:23 -0800

    refactor: align formatting and add realtime service to RestAPI struct

--- internal/restapi/rest_api.go
+++ internal/restapi/rest_api.go
@@ -5,17 +5,20 @@ import (
 	"time"
 
 	"maglev.onebusaway.org/internal/app"
+	"maglev.onebusaway.org/internal/realtime"
 )
 
 type RestAPI struct {
 	*app.Application
-	rateLimiter func(http.Handler) http.Handler
+	rateLimiter     func(http.Handler) http.Handler
+	realtimeService *realtime.Service
 }
 
 // NewRestAPI creates a new RestAPI instance with initialized rate limiter
 func NewRestAPI(app *app.Application) *RestAPI {
 	return &RestAPI{
-		Application: app,
-		rateLimiter: NewRateLimitMiddleware(app.Config.RateLimit, time.Second),
+		Application:     app,
+		rateLimiter:     NewRateLimitMiddleware(app.Config.RateLimit, time.Second),
+		realtimeService: realtime.NewService(app.GtfsManager, realtime.DefaultConfig()),
 	}
 }
