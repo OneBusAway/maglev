c256e2d0ba5579f21435d8264006b64370eacbcb
Jason <jwu0592@gmail.com>
2026-03-06 00:20:01 -0500

    fix: Update Frequency struct to match OpenAPI spec

--- internal/models/frequency.go
+++ internal/models/frequency.go
@@ -1,7 +1,10 @@
 package models
 
 type Frequency struct {
-	StartTime int64 `json:"startTime"`
-	EndTime   int64 `json:"endTime"`
-	Headway   int   `json:"headway"`
+	StartTime   int64  `json:"startTime"`
+	EndTime     int64  `json:"endTime"`
+	Headway     int    `json:"headway"`
+	ServiceData int64  `json:"serviceData"`
+	ServiceID   string `json:"serviceId"`
+	TripID      string `json:"tripId"`
 }
