dfa7016b4483dc7a244087b2040f26700bf8406f
Ved Patel <24bcs188@iiitdmj.ac.in>
2026-03-07 20:36:03 +0530

    fix indentation

--- internal/models/references.go
+++ internal/models/references.go
@@ -2,12 +2,12 @@ package models
 
 // ReferencesModel References model for related data
 type ReferencesModel struct {
-	Agencies   []AgencyReference      `json:"agencies"`
-	Routes     []Route                `json:"routes"`
-	Situations []Situation            `json:"situations"`
-	StopTimes  []RouteStopTime `json:"stopTimes"`
-	Stops      []Stop                 `json:"stops"`
-	Trips      []Trip                 `json:"trips"`
+	Agencies   []AgencyReference `json:"agencies"`
+	Routes     []Route           `json:"routes"`
+	Situations []Situation       `json:"situations"`
+	StopTimes  []RouteStopTime   `json:"stopTimes"`
+	Stops      []Stop            `json:"stops"`
+	Trips      []Trip            `json:"trips"`
 }
 
 // NewEmptyReferences creates a new empty References model with initialized empty slices
