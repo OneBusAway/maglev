292218758179d5416ca512ff6e575209249d169b
tejasva-vardhan <tejasvavardhans@gmail.com>
2026-03-29 16:50:25 +0530

    chore: automate sqlite driver selection via build tags (#805)

--- gtfsdb/driver_cgo.go
+++ gtfsdb/driver_cgo.go
@@ -1,7 +1,7 @@
-//go:build !purego
+//go:build !windows || cgo
 
 package gtfsdb
 
-import _ "github.com/mattn/go-sqlite3" // CGo-based SQLite driver
+import _ "github.com/mattn/go-sqlite3" // CGO SQLite; used on non-Windows and on Windows when CGO is enabled
 
 const DriverName = "sqlite3"
