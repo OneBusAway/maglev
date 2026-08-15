942e3b11b042bd7de5460fb4f5680c8a90672e2e
John Akindipe <akindipejohn2017@gmail.com>
2026-08-16 00:28:04 +0100

    fix typo in trips for route handler test

--- internal/restapi/trips_for_route_handler_test.go
+++ internal/restapi/trips_for_route_handler_test.go
@@ -758,7 +758,7 @@ func TestTripsForRouteHandler_ReferencesInclusion_EmptyList(t *testing.T) {
 // status is the only thing naming a stop: with includeSchedule=false there are
 // no stop times to collect, so closestStop and nextStop are the whole of what
 // references.stops has to resolve.
-func TestTripsForRouteHandler_StatusStopsAreReferencedx(t *testing.T) {
+func TestTripsForRouteHandler_StatusStopsAreReferenced(t *testing.T) {
 	api, cleanup := createTestApiWithRealTimeData(t, clock.RealClock{})
 	defer cleanup()
 
