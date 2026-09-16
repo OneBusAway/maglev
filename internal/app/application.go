ba17a8835b40bee45e36ada805153842c04fa988
Harini Krishnamurthy <harinik@users.noreply.github.com>
2026-09-16 20:04:00 +0000

    Remove Logger from Application struct

--- internal/app/application.go
+++ internal/app/application.go
@@ -1,8 +1,6 @@
 package app
 
 import (
-	"log/slog"
-
 	"maglev.onebusaway.org/internal/appconf"
 	"maglev.onebusaway.org/internal/clock"
 	"maglev.onebusaway.org/internal/gtfs"
@@ -10,12 +8,10 @@ import (
 )
 
 // Application holds the dependencies for our HTTP handlers, helpers,
-// and middleware. At the moment this only contains a copy of the Config struct and a
-// logger, but it will grow to include a lot more as our build progresses.
+// and middleware.
 type Application struct {
 	Config              appconf.Config
 	GtfsConfig          gtfs.Config
-	Logger              *slog.Logger
 	GtfsManager         *gtfs.Manager
 	DirectionCalculator *gtfs.AdvancedDirectionCalculator
 	Clock               clock.Clock
